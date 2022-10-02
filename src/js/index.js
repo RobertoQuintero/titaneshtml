a2c70f196caec259913eb47fd76249a7ce4d9d7a
Roberto Quintero <langdonhp435@gmail.com>
2022-10-02 12:16:13 -0500

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
-import toggle from './components/toggle';
-import slider from './components/slider';
-import scroll from './components/scroll';
-import lightbox from './components/lightbox'
-import WOW from './components/wow'
+import toggle from "./components/toggle";
+import slider from "./components/slider";
+import scroll from "./components/scroll";
+import lightbox from "./components/lightbox";
+import WOW from "./components/wow";
 
-new WOW().init()
-
+new WOW().init();
